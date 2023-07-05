9ce1ef1025a56060d57433f916e7140f429098d5
TrDMinh <gow.bin.gg@gmail.com>
2023-07-05 19:02:28 +0700

    Add common

---
+++ hotel-management-app/Common/Ultility.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel_management_app.Common
+{
+    public static class Ultility
+    {
+        /// <summary>
+        /// Convert object to ByteArrayContent
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static ByteArrayContent GetByteArrayContentFromObject(object obj)
+        {
+            var objByJson = JsonConvert.SerializeObject(obj);
+            var buffer = Encoding.UTF8.GetBytes(objByJson);
+            var byteContent = new ByteArrayContent(buffer);
+            return byteContent;
+        }
+    }
+}
